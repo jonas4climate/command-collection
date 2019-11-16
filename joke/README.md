d9d82e0c2f3f61ed68a7f87da9ad1e443ab848c1
Jonas Schäfer <jonas.schaefer00@gmail.com>
2019-11-16 23:02:45 +0000

    Moved from former dotfiles repo into seperate repository

---
+++ joke/README.md
@@ -0,0 +1,9 @@
+# Joke
+
+This is a command that builds an interface to get jokes printed to the terminal.
+Currently implemented are random (default), -n specific and -a all.
+
+Manpage linked to usr/local/share/man.
+
+Credits for jokes:
+https://www.dcslsoftware.com/20-one-liners-only-software-developers-understand/
---
+++ joke/joke.zsh
@@ -0,0 +1,50 @@
+#!/bin/zsh
+
+# Read jokes from joke-library.txt file
+jokes=("${(@f)$(cat ~/CS/Additional/Projects/fun-commands/joke/joke-library.txt)}")
+
+# Set defaults for flags
+a_flag="false"
+# h_flag not needed
+n_flag="false"
+n_arg=""
+
+echo ""
+
+# Read flags
+while getopts 'ahn:' flag; do
+   case "${flag}" in
+      a) a_flag="true" ;;
+      h) man joke ;;
+      n) n_flag="true"
+         n_arg="${OPTARG}" ;;
+      *) echo "Please refer to the man page for valid flags."
+         exit 1 ;;
+   esac
+done
+
+# Output according to arguments
+if [ "$#" -ne 0 ]; then
+   if [ "$a_flag" = "true" ]; then
+      for joke in $jokes; do
+         echo $joke
+         echo ""
+      done
+   elif [ "$n_flag" = "true" ]; then
+      if [ $n_arg -gt 0 ] && [ $n_arg -le $#jokes ]; then
+         echo $jokes[$n_arg]
+      else
+         echo "Out of bounds. Jokes only available between 1 and $#jokes "
+         echo "Read manpage for usage of the -n flag"
+      fi
+   else
+      echo "invalid arguments"
+      exit 1
+   fi
+else
+   # Random joke
+   min=1
+   max=$#jokes
+   random=$[${RANDOM}%max+min]
+   echo $jokes[random]
+fi
